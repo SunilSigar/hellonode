e5574521ae3f8125dc0c81fae3a2cce17277a7b8
SunilSigar <sunilsigar001@gmail.com>
2017-12-09 18:30:04 +0530

    added mongo

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var router = express.Router();
+
+mongoose.connect('mongodb://localhost/test');
+
+var mySchema = mongoose.Schema({
+    icecream: String,
+    name: String
+});
+
+var choiceModel = mongoose.model('choice', mySchema);
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'My App' });
 });
+
+
+router.get('/ilike/:icecream/:name', function(req, res) {
+  var newChoice = new choiceModel();
+  newChoice.icecream = req.params.icecream;
+  newChoice.name = req.params.name;
+  newChoice.save(function(err, savedData){
+    // res.send(savedData); 
+  });
+  choiceModel.find({},function(err, savedData){
+    res.send(savedData); 
+  });
+  
+});
+
+
+// router.get('/healthcheck', function(req, res) {
+//   var resObj = {msg: "ok"};
+//   res.send(resObj); 
+// });
+
+
 
 module.exports = router;
